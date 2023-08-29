883208295443afa7ce3eeddada128229bf6ae851
Michel Bernardo <michel_13s@outlook.com>
2023-08-29 15:19:03 -0300

    init post default

--- src/main/java/com/exemple/restspring/Application.java
+++ src/main/java/com/exemple/restspring/Application.java
@@ -1,7 +1,11 @@
 package com.exemple.restspring;
 
+import com.exemple.restspring.model.Post;
+import com.exemple.restspring.service.PostService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,9 +14,9 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-//	@Bean
-//	CommandLineRunner commandLineRunner(PostRepository repository) {
-//		return args -> repository.save(new Post("Hello World!","My first blog post!"));
-//	}
+	@Bean
+    CommandLineRunner commandLineRunner(PostService service) {
+		return args -> service.save(new Post("Hello World!","My first blog post!"));
+	}
 
 }
